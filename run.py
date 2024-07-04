948c2c6d47030a6a0cdb8e8b234b7a12ad7f4de5
Alex_Prokofev <kuisajke@gmail.com>
2024-07-04 11:20:32 +0300

    База

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import asyncio
 from functools import wraps
 import kb
 from aiogram import Bot, Dispatcher, Router, F
-from aiogram.types import  Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery
 
 router = Router()
 
@@ -10,9 +10,12 @@ router = Router()
 bot = Bot(token='6643070029:AAEoeFbuqG3730zg6Lx9M36AeKLbLhuAORA')
 dp = Dispatcher()
 dp.include_router(router)
+
+
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
 
 def check_old_answer(text):
     def decorator(func):
@@ -30,14 +33,27 @@ def check_old_answer(text):
 
 @dp.message(F.text == '/start')
 async def cmd_start(message: Message):
-    await message.answer(kb.greet.format(name=message.from_user.first_name), reply_markup=kb.keyboard1)
+    await message.answer(
+        kb.greet.format(name=message.from_user.first_name),
+        reply_markup=kb.keyboard1
+        )
 
 
 @dp.callback_query(F.data == 'big_button_1_pressed')
 @check_old_answer('big_button_1_pressed')
 async def process_button_1_press(callback: CallbackQuery):
-    await callback.message.answer(text='Была нажата БОЛЬШАЯ КНОПКА 1',reply_markup=kb.keyboard2)
+    await callback.message.answer(
+        text='Была нажата БОЛЬШАЯ КНОПКА 1', reply_markup=kb.keyboard2
+        )
+
+
+@dp.callback_query(F.data == 'big_button_2_pressed')
+@check_old_answer('big_button_2_pressed')
+async def process_button_2_press(callback: CallbackQuery):
+    await callback.message.answer(
+        text='Была нажата БОЛЬШАЯ КНОПКА 2', reply_markup=kb.keyboard3
+        )
+
 
 if __name__ == '__main__':
     asyncio.run(main())
- 
